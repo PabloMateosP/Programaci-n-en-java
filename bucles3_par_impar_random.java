8aeea8aaeb927cda8f90d01a7007d0c7a96ca61d
PabloMateosP <pmatpal0105@g.educaand.es>
2022-11-03 14:07:50 +0100

    bucles3
    Par Impar random

--- bucles3_par_impar_random.java
+++ bucles3_par_impar_random.java
@@ -1,3 +1,18 @@
+/**
+ * #auto
+ */
 public class bucles3_par_impar_random {
-    math.random
+    public static void main(String[] args) {
+        
+        int max = 100;
+        int enteroRandom = (int)Math.floor(Math.random()*max);
+
+        String par = enteroRandom + " es par ";
+        String impar = enteroRandom + " es impar ";
+
+        if (enteroRandom % 2 == 0)
+            System.out.println (par);
+        else 
+            System.out.println (impar);
+    }
 }
