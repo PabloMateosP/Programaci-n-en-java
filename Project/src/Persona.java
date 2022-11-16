fc0e071a241f5138944ab855208f22d50d974a64
PabloMateosP <pmatpal0105@g.educaand.es>
2022-11-16 09:56:44 +0100

    getter and setters

--- Project/src/Persona.java
+++ Project/src/Persona.java
@@ -1,12 +1,40 @@
-class Persona {
-    String name;
-    Integer age;
-    static String especie = "Humano";
+import javax.print.DocFlavor;
 
-    Persona (String name, Integer age) {
+class Persona {
+    private String name;
+    private Integer age;
+    private static String especie = "Humano";
+
+    public static String getEspecie() {
+        return especie;
+    }
+
+    public static void setEspecie(String especie) {
+        Persona.especie = especie;
+    }
+
+    Persona (){
+
+    }
+
+    Persona (String name, Integer age, String especie) {
         this.name = name;
         this.age = age;
         this.especie = especie;
+    }
+    //getter de name and later setter
+    String getName () {
+        return name;
+    }
+    void setName (String name) {
+        this.name = name;
+    }
+    //getter de age and later setter
+    Integer getAge () {
+        return age;
+    }
+    void setAge (Integer age) {
+        this.age = age;
     }
 
 }
--- Project/src/Principal.java
+++ Project/src/Principal.java
@@ -1,15 +1,23 @@
+import java.sql.SQLOutput;
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hi world");
+
+        Persona fulanito = new Persona();
+        fulanito.setName("Fulanito");
+        System.out.println("Su nombre: " + fulanito.getName());
         // Creamos una isntancia de Persona: Juanito 20 años.
-        Persona juanito = new Persona("Juanito", 20);
-        Persona julian = new Persona("Julian", 17);
-        System.out.println("Su nombre " + juanito.name);
-        System.out.println("Su edad: " + juanito.age);
-        System.out.println("Su especie: " + juanito.especie);
-        System.out.println("Su nombre " + julian.name);
-        System.out.println("Su edad: " + julian.age);
-        System.out.println("Su especie: " + julian.especie);
+        Persona juanito = new Persona("Juanito", 20, "Humano");
+        Persona julian = new Persona("Julian", 17,"Humano");
+
+        //we wrote the created values
+        System.out.println("Su nombre " + juanito.getName());
+        System.out.println("Su edad: " + juanito.getAge());
+        System.out.println("Su especie: " + juanito.getEspecie());
+        System.out.println("Su nombre " + julian.getName());
+        System.out.println("Su edad: " + julian.getAge());
+        System.out.println("Su especie: " + julian.getEspecie());
 
     }
 }
