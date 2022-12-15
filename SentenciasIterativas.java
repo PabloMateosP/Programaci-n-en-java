49ae901a099b8695f704604e428117d3e0bfabe1
Pablo Mateos Palas <115530158+PabloMateosP@users.noreply.github.com>
2022-12-15 18:17:21 +0100

    Delete SentenciasIterativas.java

--- SentenciasIterativas.java
+++
@@ -1,9 +0,0 @@
-public class SentenciasIterativas {
-    public static void main(String[] args) {
-        int n = 1;
-        while (n <= 5){
-            System.out.println(n);
-            n = n + 1;
-        }
-    }
-}
